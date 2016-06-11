f166785557f8a812d11217b99860b9f86de242a2
gerry <weirich@elexis.ch>
2016-06-11 11:56:21 +0200

    details

--- ungrad-labview/src/ch/elexis/ungrad/labview/controller/FullContentProvider.java
+++ ungrad-labview/src/ch/elexis/ungrad/labview/controller/FullContentProvider.java
@@ -1,11 +1,11 @@
 package ch.elexis.ungrad.labview.controller;
 
-import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.Viewer;
 
 import ch.elexis.ungrad.labview.model.LabResultsSheet;
 
-class FullContentProvider implements IStructuredContentProvider {
+class FullContentProvider implements ITreeContentProvider {
 	LabResultsSheet lrs;
 	
 	public FullContentProvider(LabResultsSheet lrs) {
@@ -20,5 +20,18 @@ class FullContentProvider implements IStructuredContentProvider {
 	@Override
 	public Object[] getElements(Object inputElement) {
 		return lrs.getItems();
+	}
+	@Override
+	public Object[] getChildren(Object parentElement){
+		return new Object[0];
+	}
+	@Override
+	public Object getParent(Object element){
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public boolean hasChildren(Object element){
+		return false;
 	}
 }
