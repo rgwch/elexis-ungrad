700a2f2e590a373af2a285096086d8efc22a0400
rgwch <rgw@elexis.ch>
2020-04-25 08:29:38 +0200

    details

--- ch.elexis.ungrad.qrbills/src/ch/elexis/ungrad/qrbills/QR_Generator.java
+++ ch.elexis.ungrad.qrbills/src/ch/elexis/ungrad/qrbills/QR_Generator.java
@@ -29,6 +29,7 @@ public class QR_Generator {
 	
 	/* ISO 20022 Defaults */
 	/* See https://www.paymentstandards.ch/dam/downloads/ig-qr-bill-de.pdf */
+	/* https://www.paymentstandards.ch/de/shared/communication-grid.html */
 	private static final QrCode.Ecc ecc = Ecc.MEDIUM;
 	private static final int VERSION = 25;
 	// private static final int size = 117; // total size must be 46x46 mm
