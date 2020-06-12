c569cce3a39d0a0005be747b453ca53c47270833
gerry <weirich@elexis.ch>
2020-06-12 13:59:50 +0200

    replacement JsonObject

--- ungrad-docmgr-lucinda/src/ch/elexis/ungrad/lucinda/JsonObject.java
+++ ungrad-docmgr-lucinda/src/ch/elexis/ungrad/lucinda/JsonObject.java
@@ -1,5 +1,15 @@
 package ch.elexis.ungrad.lucinda;
 
-public class JsonObject {
+import java.util.HashMap;
+import java.util.Map;
 
+public class JsonObject {
+	Map<String,String> cnt=new HashMap<>();
+	
+	public void put(String key, String value) {
+		cnt.put(key, value);
+	}
+	public String get(String key) {
+		return cnt.get(key);
+	}
 }
