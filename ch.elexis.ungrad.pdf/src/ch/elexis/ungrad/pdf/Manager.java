7b568745c4a02077d1b972b352452d39a8469f08
rgwch <weirich@elexis.ch>
2022-10-10 20:28:21 +0200

    initial import

---
+++ ch.elexis.ungrad.pdf/src/ch/elexis/ungrad/pdf/Manager.java
@@ -0,0 +1,13 @@
+package ch.elexis.ungrad.pdf;
+
+import java.io.File;
+
+public class Manager {
+	public void createPDF(String html, String outputPathName){
+		
+	}
+	
+	public void printFromPDF(File pdf, String printer) {
+		
+	}
+}
