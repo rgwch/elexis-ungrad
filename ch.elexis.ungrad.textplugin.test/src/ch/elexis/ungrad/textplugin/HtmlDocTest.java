469f3dd1de4003c3ea2a7973092276a3fab1b9b3
rgwch <weirich@elexis.ch>
2022-10-12 21:49:13 +0200

    added test

---
+++ ch.elexis.ungrad.textplugin.test/src/ch/elexis/ungrad/textplugin/HtmlDocTest.java
@@ -0,0 +1,24 @@
+package ch.elexis.ungrad.textplugin;
+
+import java.util.Map.Entry;
+
+import org.junit.Test;
+
+public class HtmlDocTest {
+	
+	@Test
+	public void parse() throws Exception{
+		HtmlDoc hDoc=new HtmlDoc();
+		hDoc.loadTemplate("test.html","rsc");
+		for(Entry<String, String> e: hDoc.fields.entrySet()) {
+			System.out.println(e.getKey()+"-> "+e.getValue());
+		}
+		hDoc.applyMatcher("\\[\\w+\\]", t->{
+			return t.substring(1,t.length()-1);
+		});
+		for(Entry<String, String> e: hDoc.fields.entrySet()) {
+			System.out.println(e.getKey()+"-> "+e.getValue());
+		}
+		System.out.println(hDoc.compile());
+	}
+}
