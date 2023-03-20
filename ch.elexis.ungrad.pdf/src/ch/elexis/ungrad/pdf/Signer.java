7d1d4c43a7f8a041fb1c582a9911699dd957dc91
gerry <weirich@elexis.ch>
2023-03-20 17:09:42 +0100

    div

--- ch.elexis.ungrad.pdf/src/ch/elexis/ungrad/pdf/Signer.java
+++ ch.elexis.ungrad.pdf/src/ch/elexis/ungrad/pdf/Signer.java
@@ -1,38 +1,44 @@
 package ch.elexis.ungrad.pdf;
 
-//Adding Image in Existing PDF using Java
-
-//Importing openCV libraries
 import java.io.File;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
-
-import ch.rgw.io.FileTool;
 
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import java.io.IOException;
 
 public class Signer {
 
-  public void sign(String pdfFile, String imgFile, int x, int y) throws IOException {
+	/**
+	 * Add Image to existing PDF and write resulting PDF to same directory as
+	 * name_signed.pdf
+	 * 
+	 * @param pdfFile full path to existing pdf
+	 * @param imgFile full path to image file
+	 * @param x       placement horizontal
+	 * @param y       placement vertical
+	 * @throws IOException
+	 */
+	public void sign(String pdfName, String imgFile, int x, int y) throws IOException {
+		File pdfFile = new File(pdfName);
+		PDDocument doc = PDDocument.load(pdfFile);
+		PDPage page = doc.getPage(0);
 
-    PDDocument doc = PDDocument.load(new File(pdfFile));
-    PDPage page = doc.getPage(0);
+		PDImageXObject pdfimg = PDImageXObject.createFromFile(imgFile, doc);
 
-    PDImageXObject pdfimg = PDImageXObject.createFromFile(imgFile, doc);
+		PDPageContentStream image = new PDPageContentStream(doc, page, PDPageContentStream.AppendMode.APPEND, false);
 
-    PDPageContentStream image = new PDPageContentStream(doc, page, PDPageContentStream.AppendMode.APPEND, false);
+		image.drawImage(pdfimg, x, y);
 
-    image.drawImage(pdfimg, x, y);
-  
-    image.close();
-    String path = FileTool.getFilepath(pdfFile);
-    String base = FileTool.getNakedFilename(pdfFile);
-    File out = new File(path, base + "_signed.pdf");
-    doc.save(out);
+		image.close();
 
-    // Closing the Document
-    doc.close();
-  }
+		int idx = pdfName.lastIndexOf(".");
+		if (idx > -1) {
+			pdfName = pdfName.substring(0, idx);
+		}
+		File out = new File(pdfName + "_signed.pdf");
+		doc.save(out);
+		doc.close();
+	}
 }
