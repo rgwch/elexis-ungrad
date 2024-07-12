d6fd2574c12914484ac1b1864828e5692a88e587
rgwch <weirich@elexis.ch>
2024-07-12 07:04:35 +0200

    tarmeddetails45

--- ch.elexis.ungrad.qrbills/src/ch/elexis/ungrad/qrbills/TarmedBillDetails45.java
+++ ch.elexis.ungrad.qrbills/src/ch/elexis/ungrad/qrbills/TarmedBillDetails45.java
@@ -1,13 +1,19 @@
 package ch.elexis.ungrad.qrbills;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.LoggerFactory;
 import org.xml.sax.SAXException;
+import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import ch.elexis.TarmedRechnung.Messages;
 import ch.elexis.TarmedRechnung.XMLExporter;
@@ -21,12 +27,17 @@ import ch.elexis.data.Rechnung;
 import ch.elexis.data.RnStatus;
 import ch.elexis.ungrad.qrbills.preferences.PreferenceConstants;
 import ch.fd.invoice440.request.BalanceType;
+import ch.fd.invoice440.request.BodyType;
+import ch.fd.invoice440.request.InvoiceType;
 import ch.fd.invoice440.request.ReminderType;
 import ch.fd.invoice440.request.RequestType;
 import ch.fd.invoice440.request.ServicesType;
 import ch.fd.invoice440.request.TreatmentType;
 import ch.rgw.tools.Money;
 import ch.rgw.tools.StringTool;
+import ch.rgw.tools.TimeTool;
+import at.medevit.elexis.tarmed.model.TarmedJaxbUtil;
+import at.medevit.elexis.tarmed.model.Constants;
 
 public class TarmedBillDetails45 extends QRBillDetails {
 	public static int FALL_UVG = 1;
@@ -55,9 +66,10 @@ public class TarmedBillDetails45 extends QRBillDetails {
 	String tcCode;
 	String remarks;
 	int numCons;
-	ServicesType services;
+	ch.fd.invoice450.request.ServicesType services;
 	BalanceType balance;
 	Document xmldom;
+	private static Logger logger = LoggerFactory.getLogger(TarmedBillDetails45.class);
 
 	public TarmedBillDetails45(Rechnung rn, TYPE type, boolean bStrict) throws Exception {
 		super(rn);
@@ -68,7 +80,25 @@ public class TarmedBillDetails45 extends QRBillDetails {
 			throw new Exception("Fehler in Rechnung " + rn.getNr());
 		}
 		File xmlfile = new File(outputDirXML, rn.getNr() + ".xml");
-		xmldom=readDom(xmlfile);
+		xmldom = readDom(xmlfile);
+		ch.fd.invoice450.request.RequestType request = TarmedJaxbUtil
+				.unmarshalInvoiceRequest450(new FileInputStream(xmlfile));
+		if (request == null) {
+			logger.error("Could not unmarshall xml document for invoice");
+			throw new Exception("Bad xml structure in " + rn.getNr());
+		}
+		ch.fd.invoice450.request.BodyType body = request.getPayload().getBody();
+		services = body.getServices();
+		ch.fd.invoice450.request.InvoiceType invoice = request.getPayload().getInvoice();
+		TimeTool date = new TimeTool(invoice.getRequestDate().toString());
+		documentId = invoice.getRequestId() + " - " + date.toString(TimeTool.DATE_GER) + " "
+				+ date.toString(TimeTool.TIME_FULL);
+		remarks = body.getRemark();
+
+		paymentMode = XMLExporter.TIERS_PAYANT;
+		if (body.getTiersGarant() != null) {
+			paymentMode = XMLExporter.TIERS_GARANT;
+		}
 		createReferences();
 	}
 
@@ -135,12 +165,27 @@ public class TarmedBillDetails45 extends QRBillDetails {
 	private Document readDom(File xmlFile) {
 		try {
 			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			return dbf.newDocumentBuilder()
-					.parse(VirtualFilesystemServiceHolder.get().of(xmlFile).openInputStream());
+			return dbf.newDocumentBuilder().parse(VirtualFilesystemServiceHolder.get().of(xmlFile).openInputStream());
 		} catch (ParserConfigurationException | SAXException | IOException e) {
 			LoggerFactory.getLogger(getClass()).error("Error parsing XML", e); //$NON-NLS-1$
 		}
 		return null;
+	}
+
+	protected String getXmlVersion() {
+		Attr attr = xmldom.getDocumentElement().getAttributeNode("xsi:schemaLocation"); //$NON-NLS-1$
+		String location = null;
+		if (attr != null) {
+			location = attr.getValue();
+			if (location.contains("InvoiceRequest_400")) {//$NON-NLS-1$
+				return "4.0";//$NON-NLS-1$
+			} else if (location.contains("InvoiceRequest_440")) {//$NON-NLS-1$
+				return "4.4";//$NON-NLS-1$
+			} else if (location.contains("InvoiceRequest_450")) {//$NON-NLS-1$
+				return "4.5";//$NON-NLS-1$
+			}
+		}
+		return StringUtils.EMPTY;
 	}
 
 }
