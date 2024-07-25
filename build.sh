b45c5cebfd2eab757ed26eaec30c46997966c66c
rgwch <weirich@elexis.ch>
2024-07-25 06:51:40 +0200

    parametrized build

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
-mvn -V clean verify -Dtycho.localArtifacts=ignore -Dmaven.test.skip=true -Delexis_root=`pwd`/..
+CORE=file:~/dev/elexis-3.12-builds/core-repository
+BASE=file:~/dev/elexis-3.12-builds/base-repository
+TARGET=https://download.elexis.info/elexis/target/2023-09-java17/
+mvn -V clean verify -Dtycho.localArtifacts=ignore -Dmaven.test.skip=true -DTARGET=$TARGET -DCORE=$CORE -DBASE=$BASE
+
