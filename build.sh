e8108bfcb61f21f6607339f592b7bda09f3a7a23
gerry <weirich@elexis.ch>
2024-08-15 16:29:37 +0200

    prefer external dependencies

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
-CORE=file:~/dev/elexis-3.12-builds/core-repository
-BASE=file:~/dev/elexis-3.12-builds/base-repository
+#CORE=file:~/dev/elexis-3.12-builds/core-repository
+#BASE=file:~/dev/elexis-3.12-builds/base-repository
+CORE=https://elexis.ch/ungrad2024/3.12/core/
+BASE=https://elexis.ch/ungrad2024/3.12/base/
 TARGET=https://download.elexis.info/elexis/target/2023-09-java17/
 mvn -V clean verify -Dtycho.localArtifacts=ignore -Dmaven.test.skip=true -DTARGET=$TARGET -DCORE=$CORE -DBASE=$BASE
 
