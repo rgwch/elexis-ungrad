30a9429ea68758e5a17b039fe525f9745d9621ce
Gerry Weirich <gerry@elexisbuilder.lan>
2025-12-07 13:17:33 +0000

    build script

--- build-all.sh
+++ build-all.sh
@@ -3,10 +3,14 @@
 set -o errexit
 
 cd ../elexis-3-core
+ git pull
  mvn -V clean verify  -Dtycho.localArtifacts=ignore -DskipTests -Dmaterialize-products
 
 cd ../elexis-3-base
+ git pull
  mvn -V clean verify  -Dtycho.localArtifacts=ignore -DskipTests 
 
 cd ../elexis-ungrad-plugins
+ git pull
  mvn -V clean verify  -Dtycho.localArtifacts=ignore -DskipTests 
+
