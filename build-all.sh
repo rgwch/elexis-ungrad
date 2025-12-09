a153c0bed0717efa918380e46d53acc2f2021224
weirich@elexis.ch <weirich@elexis.ch>
2025-12-09 16:50:47 +0000

    start tardoc feature

--- build-all.sh
+++ build-all.sh
@@ -3,14 +3,14 @@
 set -o errexit
 
 cd ../elexis-3-core
- git pull
+ git pull pinas ungrad-2026
  mvn -V clean verify  -Dtycho.localArtifacts=ignore -DskipTests -Dmaterialize-products
 
 cd ../elexis-3-base
- git pull
+ git pull pinas ungrad-2026
  mvn -V clean verify  -Dtycho.localArtifacts=ignore -DskipTests 
 
 cd ../elexis-ungrad-plugins
- git pull
+ git pull pinas ungrad-2026
  mvn -V clean verify  -Dtycho.localArtifacts=ignore -DskipTests 
 
