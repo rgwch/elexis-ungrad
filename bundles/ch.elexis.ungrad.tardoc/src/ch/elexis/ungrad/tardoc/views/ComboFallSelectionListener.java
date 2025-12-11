76c37609709c7d648eb9046bff183766eb82ef83
weirich@elexis.ch <weirich@elexis.ch>
2025-12-11 17:06:24 +0000

    Try plug in to konslistener

---
+++ bundles/ch.elexis.ungrad.tardoc/src/ch/elexis/ungrad/tardoc/views/ComboFallSelectionListener.java
@@ -0,0 +1,79 @@
+package ch.elexis.ungrad.tardoc.views;
+
+import java.text.MessageFormat;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.window.Window;
+
+import ch.elexis.core.model.ICoverage;
+import ch.elexis.core.model.IEncounter;
+import ch.elexis.core.ui.icons.ImageSize;
+import ch.elexis.core.ui.icons.Images;
+import ch.elexis.core.ui.services.EncounterServiceHolder;
+import ch.elexis.core.ui.util.SWTHelper;
+import ch.elexis.core.ui.views.Messages;
+import ch.rgw.tools.Result;
+
+public class ComboFallSelectionListener implements ISelectionChangedListener {
+	private boolean ignoreEventSelectionChanged;
+	private TardocKonsView view;
+
+	public ComboFallSelectionListener(TardocKonsView view) {
+		this.ignoreEventSelectionChanged = false;
+		this.view = view;
+	}
+	public void ignoreSelectionEventOnce() {
+		this.ignoreEventSelectionChanged = true;
+	}
+
+	@Override
+	public void selectionChanged(SelectionChangedEvent event) {
+		if (!ignoreEventSelectionChanged) {
+			ISelection selection = event.getSelection();
+			if (selection instanceof StructuredSelection) {
+				if (!selection.isEmpty()) {
+					ICoverage changeToCoverage = (ICoverage) ((StructuredSelection) selection).getFirstElement();
+
+					ICoverage actCoverage = null;
+					String fallLabel = "Current Case NOT found!!";//$NON-NLS-1$
+					if (view.actEncounter != null) {
+						actCoverage = view.actEncounter.getCoverage();
+						fallLabel = actCoverage.getLabel();
+					}
+
+					if (!changeToCoverage.equals(actCoverage)) {
+						if (!changeToCoverage.isOpen()) {
+							SWTHelper.alert(Messages.Core_Case_is_closed, // $NON-NLS-1$
+									Messages.KonsDetailView_CaseClosedBody); // $NON-NLS-1$
+						} else {
+							MessageDialog msd = new MessageDialog(view.getViewSite().getShell(),
+									Messages.KonsDetailView_ChangeCaseCaption, // $NON-NLS-1$
+									Images.IMG_LOGO.getImage(ImageSize._75x66_TitleDialogIconSize),
+									MessageFormat.format(Messages.KonsDetailView_ConfirmChangeConsToCase,
+											new Object[] { fallLabel, changeToCoverage.getLabel() }),
+									MessageDialog.QUESTION, new String[] { Messages.Core_Yes, // $NON-NLS-1$
+											Messages.Corr_No },
+									0); // $NON-NLS-1$
+							if (msd.open() == Window.OK) {
+								Result<IEncounter> transferResult = EncounterServiceHolder.get()
+										.transferToCoverage(view.actEncounter, changeToCoverage, false);
+								if (!transferResult.isOK()) {
+									SWTHelper.alert("Error", transferResult.toString());
+								}
+
+							} else {
+								ignoreSelectionEventOnce();
+								view.tableComboViewerFall.setSelection(new StructuredSelection(actCoverage));
+							}
+						}
+					}
+				}
+			}
+		}
+		ignoreEventSelectionChanged = false;
+	}
+}
--- bundles/ch.elexis.ungrad.tardoc/src/ch/elexis/ungrad/tardoc/views/TardocKonsView.java
+++ bundles/ch.elexis.ungrad.tardoc/src/ch/elexis/ungrad/tardoc/views/TardocKonsView.java
@@ -1,10 +1,16 @@
 package ch.elexis.ungrad.tardoc.views;
 
+import java.util.Collections;
 import java.util.Hashtable;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.ui.di.UIEventTopic;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.nebula.jface.tablecomboviewer.TableComboViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.events.SelectionAdapter;
@@ -23,13 +29,27 @@ import org.eclipse.ui.forms.widgets.Hyperlink;
 import org.eclipse.ui.part.ViewPart;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
+import org.slf4j.LoggerFactory;
 
+import ch.elexis.core.common.ElexisEventTopics;
+import ch.elexis.core.model.ICoverage;
 import ch.elexis.core.model.IEncounter;
 import ch.elexis.core.model.IPatient;
+import ch.elexis.core.model.IUser;
+import ch.elexis.core.data.service.ContextServiceHolder;
+import ch.elexis.core.ui.services.EncounterServiceHolder;
+
+import ch.elexis.core.ui.events.RefreshingPartListener;
 import ch.elexis.core.ui.text.EnhancedTextField;
+import ch.elexis.core.ui.util.CoverageComparator;
 import ch.elexis.core.ui.util.IKonsExtension;
+import ch.elexis.core.ui.views.Messages;
 import ch.elexis.ungrad.tardoc.services.TardocManager;
 import ch.elexis.ungrad.tardoc.services.TardocManagerHolder;
+import ch.rgw.tools.StringTool;
+import ch.rgw.tools.TimeTool;
+import ch.rgw.tools.VersionedResource;
+import ch.rgw.tools.VersionedResource.ResourceItem;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
@@ -49,11 +69,15 @@ public class TardocKonsView extends ViewPart {
 	Hashtable<String, IKonsExtension> hXrefs;
 	EnhancedTextField text;
 	private Hyperlink hlMandant, hlDate;
-	// TableComboViewer tableComboViewerFall;
-	private IEncounter actEncounter;
+	TableComboViewer tableComboViewerFall;
+	private ComboFallSelectionListener comboFallSelectionListener;
+
+	protected IEncounter actEncounter;
 	FormToolkit tk;
 	Form form;
 	IPatient actPat;
+	private boolean created = false;
+	
 
 	// Timer components
 	private Label timerLabel;
@@ -66,13 +90,163 @@ public class TardocKonsView extends ViewPart {
 	private Runnable timerRunnable;
 
 	// Text area
-	private Text textArea;
 
 	// Billing positions
 	private BillingPositionsComposite billingPositionsComposite;
 	private SashForm sashForm;
 	private boolean billingPositionsVisible = true;
 	private Action toggleBillingPositionsAction;
+	
+	private RefreshingPartListener udpateOnVisible = new RefreshingPartListener(this) {
+
+		@Override
+		public void partActivated(org.eclipse.ui.IWorkbenchPartReference partRef) {
+			if (isMatchingPart(partRef)) {
+				if (actEncounter != null && !text.isDirty()) {
+					setKonsText(actEncounter, actEncounter.getVersionedEntry().getHeadVersion());
+				}
+			}
+		};
+
+		@Override
+		public void partDeactivated(org.eclipse.ui.IWorkbenchPartReference partRef) {
+			if (isMatchingPart(partRef)) {
+				// save entry on deactivation if text was edited
+				if (actEncounter != null && (text.isDirty())) {
+					EncounterServiceHolder.get().updateVersionedEntry(actEncounter, text.getContentsAsXML(),
+							getVersionRemark());
+					text.setDirty(false);
+					ContextServiceHolder.get().postEvent(ElexisEventTopics.EVENT_UPDATE, actEncounter);
+				}
+			}
+		};
+
+		@Override
+		public void partVisible(org.eclipse.ui.IWorkbenchPartReference partRef) {
+			// do not call super and refresh, should be handled by partActivated
+			if (isMatchingPart(partRef)) {
+				// adaptMenus();
+			}
+		};
+	};
+
+	@Optional
+	@Inject
+	void udpatePatient(@UIEventTopic(ElexisEventTopics.EVENT_UPDATE) IPatient patient) {
+		if (patient != null && patient.equals(actPat) && created) {
+			actPat = null; // make sure patient will be updated
+			setPatient(patient);
+		}
+	}
+
+	@Inject
+	@Optional
+	public void reloadPatient(@UIEventTopic(ElexisEventTopics.EVENT_RELOAD) Class<?> clazz) {
+		if (IPatient.class.equals(clazz) && created) {
+			actPat = null; // make sure patient will be updated
+			setPatient(actPat);
+		}
+	}
+
+	@Inject
+	void activePatient(@Optional IPatient patient) {
+		if (created) {
+			Display.getDefault().asyncExec(() -> {
+				actPat = null; // make sure patient will be updated
+				setPatient(patient);
+			});
+		}
+	}
+
+	@Inject
+	void activeUser(@Optional IUser user) {
+		if (created) {
+			Display.getDefault().asyncExec(() -> {
+				//adaptMenus();
+			});
+		}
+	}
+
+	@Inject
+	@Optional
+	public void reloadCoverage(@UIEventTopic(ElexisEventTopics.EVENT_RELOAD) Class<?> clazz) {
+		if (ICoverage.class.equals(clazz) && created) {
+			updateFallCombo();
+		}
+	}
+
+	@Inject
+	void updateCoverage(@Optional @UIEventTopic(ElexisEventTopics.EVENT_UPDATE) ICoverage coverage) {
+		if (created) {
+			updateFallCombo();
+		}
+	}
+
+
+	private String getVersionRemark() {
+		String remark = "edit"; //$NON-NLS-1$
+		java.util.Optional<IUser> activeUser = ContextServiceHolder.get().getRootContext().getTyped(IUser.class);
+		if (activeUser.isPresent()) {
+			remark = activeUser.get().getLabel();
+		}
+		return remark;
+	}
+	
+	/** Aktuellen patient setzen */
+	private synchronized void setPatient(IPatient pat) {
+		LoggerFactory.getLogger(getClass()).info("[KONS PAT] " + (pat != null ? pat.getId() : "null"));
+		if (pat != null && actPat != null) {
+			if (pat.getId().equals(actPat.getId())) {
+				if (!form.getText().equals(Messages.KonsDetailView_NoConsSelected)) {
+					return;
+				}
+			}
+		}
+
+		actPat = pat;
+		if (pat != null) {
+			form.setText(pat.getLabel() + StringTool.space + "(" + pat.getAgeInYears() + ")"); //$NON-NLS-1$ //$NON-NLS-2$
+			// stickerComposite.setPatient(pat);
+			updateFallCombo();
+		}
+		form.layout();
+	}
+
+	private void updateFallCombo() {
+	    IPatient pat = ContextServiceHolder.get().getRootContext().getTyped(IPatient.class).orElse(null);
+	    if (pat != null && tableComboViewerFall != null) {
+	        List<ICoverage> coverages = pat.getCoverages();
+	        Collections.sort(coverages, new CoverageComparator());
+	        tableComboViewerFall.setInput(coverages);
+	        if (actEncounter != null) {
+	            comboFallSelectionListener.ignoreSelectionEventOnce();
+	            tableComboViewerFall.setSelection(new StructuredSelection(actEncounter.getCoverage()));
+	        }
+			// needed for initial background colours
+			tableComboViewerFall.getTableCombo().setTableVisible(true);
+			tableComboViewerFall.getTableCombo().setTableVisible(false);
+			tableComboViewerFall.refresh();
+	    }
+	}
+	void setKonsText(final IEncounter encounter, final int version) {
+		String ntext = StringUtils.EMPTY;
+		if ((version >= 0) && (version <= encounter.getVersionedEntry().getHeadVersion())) {
+			VersionedResource vr = encounter.getVersionedEntry();
+			ResourceItem entry = vr.getVersion(version);
+			ntext = entry.data;
+			StringBuilder sb = new StringBuilder();
+			sb.append("rev. ").append(version).append(Messages.KonsDetailView_of) //$NON-NLS-1$
+					.append( // $NON-NLS-2$
+							new TimeTool(entry.timestamp).toString(TimeTool.FULL_GER))
+					.append(" (").append(entry.remark).append(")"); //$NON-NLS-1$ //$NON-NLS-2$
+		}	
+		text.setText(ntext);
+		text.setKons(encounter);
+		// displayedVersion = version;
+		// versionBackAction.setEnabled(version != 0);
+		// versionFwdAction.setEnabled(version != encounter.getVersionedEntry().getHeadVersion());
+	}
+
 
 	@Override
 	public void createPartControl(Composite parent) {
@@ -174,9 +348,9 @@ public class TardocKonsView extends ViewPart {
 		textGroup.setLayout(new GridLayout(1, false));
 		textGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 
-		textArea = new Text(textGroup, SWT.MULTI | SWT.BORDER | SWT.WRAP | SWT.V_SCROLL);
-		textArea.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		textArea.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
+		text = new EnhancedTextField(textGroup, SWT.MULTI | SWT.BORDER | SWT.WRAP | SWT.V_SCROLL);
+		text.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		text.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
 	}
 
 	/**
@@ -328,8 +502,8 @@ public class TardocKonsView extends ViewPart {
 
 	@Override
 	public void setFocus() {
-		if (textArea != null && !textArea.isDisposed()) {
-			textArea.setFocus();
+		if (text != null && !text.isDisposed()) {
+			text.setFocus();
 		}
 	}
 
