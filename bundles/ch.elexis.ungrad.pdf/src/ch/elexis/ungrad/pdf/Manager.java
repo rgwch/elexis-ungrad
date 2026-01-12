acc37171ec727858ddfdbb1c4c8e07de2ae6addf
weirich@elexis.ch <weirich@elexis.ch>
2026-01-12 20:19:34 +0000

    eliminate race conditions

--- bundles/ch.elexis.ungrad.pdf/src/ch/elexis/ungrad/pdf/Manager.java
+++ bundles/ch.elexis.ungrad.pdf/src/ch/elexis/ungrad/pdf/Manager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2022-2024, G. Weirich and Elexis
+ * Copyright (c) 2022-2026, G. Weirich and Elexis
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -37,35 +37,36 @@ import ch.rgw.tools.StringTool;
 
 /**
  * Handle HTML-to-PDF conversion, access PDF form fields, and print PDF files
- * Uses https://github.com/danfickle/openhtmltopdf and https://pdfbox.apache.org/
+ * Uses https://github.com/danfickle/openhtmltopdf and
+ * https://pdfbox.apache.org/
+ * 
  * @author gerry
  *
  */
 public class Manager {
-	private PDDocument pdfDoc;
-	
+
 	/**
-	 * Fill out a PDF form. 
-	 * @param formpath full path to the pdf file with an embedded form
+	 * Fill out a PDF form.
+	 * 
+	 * @param formpath   full path to the pdf file with an embedded form
 	 * @param outputPath full lath for the resulting file with filled form
-	 * @param fields fields to write. fields in the form not mentioned herein remain simply unchanged. Fields not found in the form are ignored.
+	 * @param fields     fields to write. fields in the form not mentioned herein
+	 *                   remain simply unchanged. Fields not found in the form are
+	 *                   ignored.
 	 * @return the path to the newly written file (which is the same as outputPath)
 	 * @throws Exception
 	 */
-	public String fillForm(String formpath, String outputPath, Map<String, String> fields)
-		throws Exception{
+	public String fillForm(String formpath, String outputPath, Map<String, String> fields) throws Exception {
 		try {
-			if (pdfDoc == null) {
-				InputStream resource = new FileInputStream(formpath);
-				pdfDoc = PDDocument.load(resource);
-			}
+			InputStream resource = new FileInputStream(formpath);
+			PDDocument pdfDoc = PDDocument.load(resource);
 			PDDocumentCatalog docCatalog = pdfDoc.getDocumentCatalog();
 			pdfDoc.getDocumentInformation().setCreator("Elexis Ungrad");
 			pdfDoc.getDocumentInformation().setCustomMetadataValue("concern", "Forms");
 			PDAcroForm acroForm = docCatalog.getAcroForm();
 			for (Entry<String, String> e : fields.entrySet()) {
-				String key=e.getKey();
-				String value=e.getValue();
+				String key = e.getKey();
+				String value = e.getValue();
 				PDField pdField = acroForm.getField(key);
 				if (pdField != null) {
 					try {
@@ -84,18 +85,17 @@ public class Manager {
 			return "";
 		}
 	}
-	
+
 	/**
 	 * Retrieve the value of a field in a form embedded in a pdf file
-	 * @param formPath full filepath to the PDF File with an embedded form
+	 * 
+	 * @param formPath  full filepath to the PDF File with an embedded form
 	 * @param fieldName Name of the field to retrieve
 	 * @return the contents of the field or empty string if no such field was found.
 	 * @throws Exception
 	 */
-	public String getFieldContents(String formPath, String fieldName) throws Exception{
-		if (pdfDoc == null) {
-			pdfDoc = PDDocument.load(new File(formPath));
-		}
+	public String getFieldContents(String formPath, String fieldName) throws Exception {
+		PDDocument pdfDoc = PDDocument.load(new File(formPath));
 		PDDocumentCatalog docCatalog = pdfDoc.getDocumentCatalog();
 		PDAcroForm acroForm = docCatalog.getAcroForm();
 		PDField pdField = acroForm.getField(fieldName);
@@ -109,34 +109,38 @@ public class Manager {
 		}
 		return "";
 	}
-	
+
 	/**
 	 * Create a PDF file from a HTML.
-	 * @param inputHtml the file to read. If a style sheet is linked, it must be in the current dir or an absolute path.
+	 * 
+	 * @param inputHtml  the file to read. If a style sheet is linked, it must be in
+	 *                   the current dir or an absolute path.
 	 * @param outputFile the PDF to write
 	 * @throws FileNotFoundException
 	 * @throws IOException
 	 * @throws PrinterException
 	 */
-	public void createPDF(File inputHtml, File outputFile)
-		throws FileNotFoundException, IOException, PrinterException{
+	public void createPDF(File inputHtml, File outputFile) throws FileNotFoundException, IOException, PrinterException {
 		FileOutputStream fout = new FileOutputStream(outputFile);
 		PdfRendererBuilder builder = new PdfRendererBuilder().useFastMode().withFile(inputHtml)
-			.withProducer("Elexis Ungrad");
+				.withProducer("Elexis Ungrad");
 		builder.toStream(fout);
 		builder.run();
-		
+		fout.close();
+		builder=null;
 	}
-	
+
 	/**
 	 * Print a PDF file
+	 * 
 	 * @param pdfFile the file to print
-	 * @param printer The printer to use. If it is null or the named printer is not found, a printer select dialog will open.
+	 * @param printer The printer to use. If it is null or the named printer is not
+	 *                found, a printer select dialog will open.
 	 * @return true after print-
 	 * @throws IOException
 	 * @throws PrinterException
 	 */
-	public boolean printFromPDF(File pdfFile, String printer) throws IOException, PrinterException{
+	public boolean printFromPDF(File pdfFile, String printer) throws IOException, PrinterException {
 		PDDocument pdoc = PDDocument.load(pdfFile);
 		PDFPrintable printable = new PDFPrintable(pdoc);
 		PrinterJob job = PrinterJob.getPrinterJob();
@@ -159,20 +163,20 @@ public class Manager {
 			attrs.add(MediaSizeName.ISO_A4);
 			job.print(attrs);
 			printed = true;
-			
+
 		} else {
 			if (job.printDialog()) {
 				job.print();
 				printed = true;
 			}
-			
+
 		}
 		pdoc.close();
 		return printed;
-		
+
 	}
-	
-	public boolean printFromHTML(File htmlFle, String printer, boolean bKeepHtml, boolean bKeepPdf){
+
+	public boolean printFromHTML(File htmlFle, String printer, boolean bKeepHtml, boolean bKeepPdf) {
 		return false;
 	}
 }
